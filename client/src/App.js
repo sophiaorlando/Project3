437b93cba432883a0a4c6aec824edf877a0a7cb9
sophiaorlando <sophia19orlando@gmail.com>
2020-11-19 23:04:23 -0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+
 import "./App.css";
 
+const api = {
+  url: "https://api.thesneakerdatabase.com/v1/sneakers?limit=10&brand=jordan&releaseDate=lte:2019-10-11&name=travis%20scott"
+}
+
+
 function App() {
+
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [sneakers, setSneakers] = useState({})
+
+  const search = () => {
+    fetch("https://api.thesneakerdatabase.com/v1/sneakers?limit=10&brand=jordan&releaseDate=lte:2019-10-11&name=travis%20scott")
+      .then(res => res.json())
+      .then((result) => {
+        setIsLoaded(true)
+        setSneakers(result)
+        console.log(result)
+      })
+  }
+
+
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <main>
+      <div className="search-box">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          // onChange={e => setQuery(e.target.value)}
+          // value={query}
+          onKeyPress={search}
+        />
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </main>
   );
 }
 
