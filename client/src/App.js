ed2c9841960587af8c125f3a64204740b0fcb079
sophiaorlando <sophia19orlando@gmail.com>
2020-11-21 13:03:44 -0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 
 import "./App.css";
 
-const api = {
-  url: "https://api.thesneakerdatabase.com/v1/sneakers?limit=10&brand=jordan&releaseDate=lte:2019-10-11&name=travis%20scott"
-}
+// const api = {
+//   url: "https://api.thesneakerdatabase.com/v1/sneakers?limit=10&brand=jordan&releaseDate=lte:2019-10-11&name=travis%20scott"
+// }
 
 
 function App() {
