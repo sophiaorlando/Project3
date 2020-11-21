cbc234d7df8c4728030b8a014c159dc19d5d3927
sophiaorlando <sophia19orlando@gmail.com>
2020-11-21 16:32:25 -0500

    updated

--- client/src/components/searchBar/searchBar.js
+++ client/src/components/searchBar/searchBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./searchBar.css"
 
+
 // import App from "../App.js"
 
 function SearchBar(){
@@ -27,7 +28,7 @@ function SearchBar(){
   };
   return(
     <div>
-    <div className="search-box">
+    <div className="search-box col">
     <input
       type="text"
       className="search-bar"
@@ -37,6 +38,33 @@ function SearchBar(){
       onKeyPress={search}
     />
   </div>
+
+  {/* <Form>
+  <Form.Group>
+    <Form.Label>Search Sneakers</Form.Label>
+    <Form.Control className="search-bar" type="text" placeholder="Search Sneaker Brands"
+          onChange={(e) => setBrand(e.target.value)}
+          value={brand}
+          onSubmit={search}
+    />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+  </Form> */}
+{/* 
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form> */}
+
         {typeof sneakers.results != "undefined" ? (
           <div>
             <h3>{sneakers.results[0].brand}</h3>
@@ -50,3 +78,19 @@ function SearchBar(){
   )
 }
 export default SearchBar;
+
+// import React from "react";
+// import { MDBCol, MDBFormInline, MDBIcon } from "mdbreact";
+
+// const SearchPage = () => {
+//   return (
+//     <MDBCol md="6">
+//       <MDBFormInline className="md-form">
+//         <MDBIcon icon="search" />
+//         <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
+//       </MDBFormInline>
+//     </MDBCol>
+//   );
+// }
+
+// export default SearchPage;
