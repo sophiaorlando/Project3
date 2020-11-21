716eda6f8fe2f8edcee5295d62f32252b4c7f28c
Cody Walter <cody.walter@providencecc.edu>
2020-11-20 17:27:40 -0800

    added AuthForm for front-end

--- client/src/App.js
+++ client/src/App.js
@@ -3,31 +3,31 @@ import React, { useState } from "react";
 import "./App.css";
 
 const api = {
-  url: "https://api.thesneakerdatabase.com/v1/sneakers?limit=10&brand=jordan&releaseDate=lte:2019-10-11&name=travis%20scott"
-}
-
+  url:
+    "https://api.thesneakerdatabase.com/v1/sneakers?limit=10&brand=jordan&releaseDate=lte:2019-10-11&name=travis%20scott",
+};
 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [brand, setBrand] = useState("");
+  const [sneakers, setSneakers] = useState({});
 
-  const [error, setError] = useState(null)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [brand, setBrand] = useState('')
-  const [sneakers, setSneakers] = useState({})
-
-  const search = evt => {
+  const search = (evt) => {
     if (evt.key === "Enter") {
-    fetch(`https://api.thesneakerdatabase.com/v1/sneakers?limit=10&brand=${brand}`)
-    //&releaseDate=lte:2019-10-11&name=travis%20scott
-      .then(res => res.json())
-      .then((result) => {
-        setIsLoaded(true)
-        setBrand('')
-        setSneakers(result)
-        console.log(result)
-      })
+      fetch(
+        `https://api.thesneakerdatabase.com/v1/sneakers?limit=10&brand=${brand}`
+      )
+        //&releaseDate=lte:2019-10-11&name=travis%20scott
+        .then((res) => res.json())
+        .then((result) => {
+          setIsLoaded(true);
+          setBrand("");
+          setSneakers(result);
+          console.log(result);
+        });
     }
-  }
-
+  };
 
   return (
     <main>
@@ -36,24 +36,24 @@ function App() {
           type="text"
           className="search-bar"
           placeholder="Search..."
-          onChange={e => setBrand(e.target.value)}
+          onChange={(e) => setBrand(e.target.value)}
           value={brand}
           onKeyPress={search}
         />
       </div>
 
-{(typeof sneakers.results != "undefined") ? (
-  <div>
-  <div>{sneakers.results[0].brand}</div> 
-  </div>
+      {typeof sneakers.results != "undefined" ? (
+        <div>
+          <div>{sneakers.results[0].brand}</div>
+        </div>
+      ) : (
+        ""
+      )}
 
-) : ('')}
-
-  {/* <div>{sneakers.results}</div> */}
-  {/* <div>{!sneakers ? sneakers.results[0].brand : "No results to display"}</div> */}
+      {/* <div>{sneakers.results}</div> */}
+      {/* <div>{!sneakers ? sneakers.results[0].brand : "No results to display"}</div> */}
     </main>
   );
 }
-
 
 export default App;
