edd6ba7c47c6cc53f9308bf2907f27559001d906
Maya Chisem <chisem.maya@gmail.com>
2020-12-10 21:20:46 -0800

    pull status

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,7 +2,7 @@ import React from "react";
 import CompJumbo from "../components/jumbotron/jumbotron";
 // import JustInShoes from "../components/justInShoes/justInShoes";
 // import ScrollUp from "../components/scrollUp/scrollUp";
-import Footer from "../components/Footer/footer";
+// import Footer from "../components/Footer/footer";
 
 import SearchBar from "../components/searchBar/searchBar";
 
